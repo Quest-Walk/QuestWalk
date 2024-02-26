d8dd010a9507269cdf07aa8de9d55f477b869af1
Ohleesang <jammmmed@gmail.com>
2024-02-26 11:05:11 +0900

    Chore : init setting - ViewBinding 활성화 - SDK 최소 조건 맞추기(34/33)

--- app/src/main/java/com/hapataka/questwalk/MainActivity.kt
+++ app/src/main/java/com/hapataka/questwalk/MainActivity.kt
@@ -2,10 +2,12 @@ package com.hapataka.questwalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hapataka.questwalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
