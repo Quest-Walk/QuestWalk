8e72c8feaba9985daa9e6fa604370b3a3627d082
YS <etgt777@naver.com>
2024-02-26 16:29:28 +0900

    UI: add record fragment - not attach tap layout, view pager

---
+++ app/src/main/java/com/hapataka/questwalk/RecordFragment.kt
@@ -0,0 +1,24 @@
+package com.hapataka.questwalk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hapataka.questwalk.databinding.FragmentRecordBinding
+
+class RecordFragment : Fragment() {
+    private val binding by lazy { FragmentRecordBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
