a8db65d4f23b9735228ffac0d7811f3c9c242c87
Ohleesang <jammmmed@gmail.com>
2024-02-26 19:58:10 +0900

    UI : Button change backgroundTintColor
    버튼 클릭시 색깔변경 추가

--- app/src/main/java/com/hapataka/questwalk/homefragment/HomeFragment.kt
+++ app/src/main/java/com/hapataka/questwalk/homefragment/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.hapataka.questwalk.homefragment
 
+import android.content.res.ColorStateList
 import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import com.hapataka.questwalk.R
 import com.hapataka.questwalk.databinding.FragmentHomeBinding
 
@@ -19,6 +23,9 @@ class HomeFragment : Fragment() {
 
     //모험 대기(wait),모험 시작(play)
     private var isPlay = false
+
+    //타이머
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -37,18 +44,37 @@ class HomeFragment : Fragment() {
     private fun initView() {
         with(binding) {
             btnQuestStatus.setOnClickListener {
-                if (!isPlay) {
+                if (!isPlay) { // 모험 시작!
                     clPlayingBottomWidgets.visibility = View.VISIBLE
                     clPlayingUpperWidgets.visibility = View.VISIBLE
                     clWaitingBottomWidgets.visibility = View.GONE
                     isPlay = true
-                } else {
+                    btnQuestStatus.text = "포기하기"
+                    setBackgroundWidget(btnQuestStatus,R.color.red)
+                } else { // 모험이 끝날때!
                     clPlayingBottomWidgets.visibility = View.GONE
                     clPlayingUpperWidgets.visibility = View.GONE
                     clWaitingBottomWidgets.visibility = View.VISIBLE
                     isPlay = false
+
+
+                    // 버튼색,이름 바꾸기
+                    btnQuestStatus.text = "모험 시작하기"
+                    setBackgroundWidget(btnQuestStatus,R.color.green)
                 }
             }
         }
+    }
+
+
+    // backgroundTint 값 변경
+    private fun setBackgroundWidget(view : View,colorResource :Int){
+        val colorStateList = ColorStateList.valueOf(
+            ContextCompat.getColor(
+                requireContext(),
+                colorResource
+            )
+        )
+        ViewCompat.setBackgroundTintList(view, colorStateList)
     }
 }
