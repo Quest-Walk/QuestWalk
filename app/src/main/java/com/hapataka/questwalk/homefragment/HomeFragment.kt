99235125c1bf11a7f934b577a62c586131151e0a
Ohleesang <jammmmed@gmail.com>
2024-02-26 16:53:46 +0900

    UI : default homefragemnt add imagebtn 각 해당 액티비티/프래그먼트로 이동하는 아이콘 구성

--- app/src/main/java/com/hapataka/questwalk/homefragment/HomeFragment.kt
+++ app/src/main/java/com/hapataka/questwalk/homefragment/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.hapataka.questwalk.homefragment
 
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,19 +15,40 @@ class HomeFragment : Fragment() {
 
 
     private lateinit var viewModel: HomeViewModel
-    private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+
+    //모험 대기(wait),모험 시작(play)
+    private var isPlay = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         // TODO: Use the ViewModel
+        initView()
     }
 
+    private fun initView() {
+        with(binding) {
+            btnQuestStatus.setOnClickListener {
+                if (!isPlay) {
+                    clPlayingBottomWidgets.visibility = View.VISIBLE
+                    clPlayingUpperWidgets.visibility = View.VISIBLE
+                    clWaitingBottomWidgets.visibility = View.GONE
+                    isPlay = true
+                } else {
+                    clPlayingBottomWidgets.visibility = View.GONE
+                    clPlayingUpperWidgets.visibility = View.GONE
+                    clWaitingBottomWidgets.visibility = View.VISIBLE
+                    isPlay = false
+                }
+            }
+        }
+    }
 }
