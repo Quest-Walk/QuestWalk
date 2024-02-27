f5e40133eec78bfa0214eb23d69bc3fd45dbafe6
Ohleesang <jammmmed@gmail.com>
2024-02-27 10:07:09 +0900

    Feat : create CameraFragment CameraFragment 생성

---
+++ app/src/main/java/com/hapataka/questwalk/camerafragment/CameraFragment.kt
@@ -0,0 +1,29 @@
+package com.hapataka.questwalk.camerafragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hapataka.questwalk.R
+
+class CameraFragment : Fragment() {
+
+
+    private lateinit var viewModel: CameraViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CameraViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/hapataka/questwalk/camerafragment/CameraViewModel.kt
@@ -0,0 +1,7 @@
+package com.hapataka.questwalk.camerafragment
+
+import androidx.lifecycle.ViewModel
+
+class CameraViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
