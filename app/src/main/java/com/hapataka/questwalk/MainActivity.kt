67f064ecdc2c3af401746bbbc5ca6c6c485656c5
이성재 <sj5lee1@gmail.com>
2024-02-28 21:36:24 +0900

    Feat: tested maps

--- app/src/main/java/com/hapataka/questwalk/MainActivity.kt
+++ app/src/main/java/com/hapataka/questwalk/MainActivity.kt
@@ -2,12 +2,12 @@ package com.hapataka.questwalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.activity.result.contract.ActivityResultContracts
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
