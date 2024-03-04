34c40730ebb2bd5233828c767573059d69828492
YS <etgt777@naver.com>
2024-03-04 17:04:53 +0900

    UI: add home design

--- app/src/main/java/com/hapataka/questwalk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.hapataka.questwalk.ui.home
 
 import android.content.res.ColorStateList
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.Chronometer
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
@@ -16,15 +20,19 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.load
+import coil.request.ImageRequest
 import com.hapataka.questwalk.R
 import com.hapataka.questwalk.camerafragment.CameraFragment
 import com.hapataka.questwalk.databinding.FragmentHomeBinding
+import com.hapataka.questwalk.ui.record.TAG
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
-
-
     private lateinit var viewModel: HomeViewModel
     private lateinit var binding: FragmentHomeBinding
     private var backPressedOnce = false
@@ -33,7 +41,7 @@ class HomeFragment : Fragment() {
     private var isPlay = false
 
     //타이머
-    private lateinit var timer : Chronometer
+    private lateinit var timer: Chronometer
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -55,6 +63,9 @@ class HomeFragment : Fragment() {
         replaceFragmentByImageButton()
         setQuestButtonEvent()
         initBackPressedCallback()
+
+        Log.i(TAG, "dis: ${getResources().getDisplayMetrics().density}")
+
     }
 
     /**
@@ -62,15 +73,15 @@ class HomeFragment : Fragment() {
      */
 
     private val navController by lazy { (parentFragment as NavHostFragment).findNavController() }
-    private fun replaceFragmentByImageButton(){
-        with(binding){
-            ibHistory.setOnClickListener {
+    private fun replaceFragmentByImageButton() {
+        with(binding) {
+            btnRecord.setOnClickListener {
                 navController.navigate(R.id.action_frag_home_to_frag_record)
             }
-            ibMyPage.setOnClickListener {
+            btnMyPage.setOnClickListener {
                 navController.navigate(R.id.action_frag_home_to_frag_my_info)
             }
-            ibWheather.setOnClickListener {
+            btnWheather.setOnClickListener {
                 // TODO : wheatherFragment 이동
             }
             ibCamera.setOnClickListener {
@@ -83,50 +94,83 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment) {
         requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_graph,fragment)
+            .replace(R.id.nav_graph, fragment)
             .addToBackStack(null)
             .commit()
     }
 
-    private fun setQuestButtonEvent(){
+    private fun setQuestButtonEvent() {
         with(binding) {
             btnQuestStatus.setOnClickListener {
                 if (!isPlay) { // 모험 시작!
                     clPlayingBottomWidgets.visibility = View.VISIBLE
-                    clPlayingUpperWidgets.visibility = View.VISIBLE
-                    clWaitingBottomWidgets.visibility = View.GONE
+                    llPlayingContents.visibility = View.VISIBLE
+                    btnQuestChange.visibility = View.GONE
                     isPlay = true
 
                     //버튼색 이름 및 색깔 변경
                     btnQuestStatus.text = "포기하기"
-                    setBackgroundWidget(btnQuestStatus,R.color.red)
+                    setBackgroundWidget(btnQuestStatus, R.color.red)
 
                     //타이머
                     timer.base = SystemClock.elapsedRealtime()
                     timer.start()
+                    initQuestStart()
                 } else { // 모험이 끝날때!
                     clPlayingBottomWidgets.visibility = View.GONE
-                    clPlayingUpperWidgets.visibility = View.GONE
-                    clWaitingBottomWidgets.visibility = View.VISIBLE
+                    llPlayingContents.visibility = View.GONE
+                    btnQuestChange.visibility = View.VISIBLE
                     isPlay = false
-
 
                     //버튼색 이름 및 색깔 변경
                     btnQuestStatus.text = "모험 시작하기"
-                    setBackgroundWidget(btnQuestStatus,R.color.green)
+                    setBackgroundWidget(btnQuestStatus, R.color.green)
 
                     //타이머 중지
                     timer.stop()
+                    initQuestEnd()
                 }
             }
         }
     }
 
+    private fun initQuestStart() {
+        val imageLoader = ImageLoader.Builder(requireContext())
+            .components {
+                if (SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
+        val request = ImageRequest.Builder(requireContext())
+            .data(R.drawable.character_move_01)
+            .target(binding.ivChrImage)
+            .build()
+
+        imageLoader.enqueue(request)
+
+        val animation = TranslateAnimation(
+            Animation.RELATIVE_TO_SELF, 1f,
+            Animation.RELATIVE_TO_SELF, 0f,
+            Animation.RELATIVE_TO_SELF, 0f,
+            Animation.RELATIVE_TO_SELF, 0f,
+            )
+
+        animation.duration = 3000
+        animation.repeatCount = Animation.INFINITE
+        binding.background.startAnimation(animation)
+    }
+
+    private fun initQuestEnd() {
+        binding.ivChrImage.load(R.drawable.character_01)
+    }
 
     // backgroundTint 값 변경
-    private fun setBackgroundWidget(view : View,colorResource :Int){
+    private fun setBackgroundWidget(view: View, colorResource: Int) {
         val colorStateList = ColorStateList.valueOf(
             ContextCompat.getColor(
                 requireContext(),
