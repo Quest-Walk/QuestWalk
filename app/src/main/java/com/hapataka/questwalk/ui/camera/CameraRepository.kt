f0062405d3a57d9e3d808c210d2ff723cb120ac8
Ohleesang <jammmmed@gmail.com>
2024-03-05 20:43:22 +0900

    feat : resized Bitmap - 크기 제한 1000 으로 인해 리사이즈 필요

--- app/src/main/java/com/hapataka/questwalk/ui/camera/CameraRepository.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/camera/CameraRepository.kt
@@ -10,15 +10,32 @@ import javax.inject.Inject
 class CameraRepository @Inject constructor(@ApplicationContext private val context: Context) {
 
     fun saveBitmap(bitmap: Bitmap, filename: String): File {
+        val resizedBitmap = resizedBitmap(bitmap,1000)
         val file = File(context.filesDir, filename)
         val fos = FileOutputStream(file)
 
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
         fos.close()
 
         return file
     }
-    fun deleteBitmap(){
+
+    private fun resizedBitmap(image: Bitmap, maxSize: Int): Bitmap {
+        var width = image.width
+        var height = image.height
+
+        val bitmapRatio = width.toFloat() / height.toFloat()
+        if (bitmapRatio > 1) {
+            width = maxSize
+            height = (width / bitmapRatio).toInt()
+        } else {
+            height = maxSize
+            width = (height * bitmapRatio).toInt()
+        }
+        return Bitmap.createScaledBitmap(image,width,height,true)
+    }
+
+    fun deleteBitmap() {
         // TODO : 이미지 처리 후 내부 저장소 에 이미지 삭제
     }
 
