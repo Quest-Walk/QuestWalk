97d648aab9dcd74ac33a42ffc2a4d8f42020a0cc
이성재 <sj5lee1@gmail.com>
2024-03-14 10:21:54 +0900

    Feat: 지도 애니메이션 구축

--- app/src/main/java/com/hapataka/questwalk/ui/result/ResultFragment.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/result/ResultFragment.kt
@@ -1,24 +1,25 @@
 package com.hapataka.questwalk.ui.result
 
+import android.animation.ValueAnimator
 import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.FOCUSABLE
 import androidx.fragment.app.viewModels
 import coil.load
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.JointType
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.PolygonOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.gms.maps.model.RoundCap
+import com.google.android.gms.maps.model.StrokeStyle
+import com.google.android.gms.maps.model.StyleSpan
 import com.hapataka.questwalk.R
 import com.hapataka.questwalk.databinding.FragmentResultBinding
 import com.hapataka.questwalk.domain.entity.HistoryEntity
@@ -132,8 +133,10 @@ class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding
     private fun updateLocation(p0: GoogleMap, result: HistoryEntity.ResultEntity) {
         Log.d("check", result.toString())
         var preLocation: Pair<Float, Float>? = null
+        var destinationLocation: Pair<Float, Float>? = null
         val resultLati = result.locations?.map { it.first } ?: listOf()
         val resultLongi = result.locations?.map { it.second } ?: listOf()
+        val resultIterator = resultLati.zip(resultLongi).iterator()
         if (resultLati.any() && resultLongi.any()){
             val cameraUpdate = CameraUpdateFactory.newLatLngBounds(
                 LatLngBounds(
@@ -144,41 +147,74 @@ class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding
             p0.animateCamera(cameraUpdate)
         }
 
-        for (location in resultLati.zip(resultLongi)) {
-            Log.d(TAG + "위치정보", "위도: ${location.first.toDouble()} 경도: ${location.second.toDouble()}")
-            if (preLocation != null) {
-                Log.d(
-                    TAG + "check",
-                    "${location.first.toDouble()} ${location.second.toDouble()} ${preLocation.first.toDouble()} ${preLocation.second.toDouble()}"
-                )
-                val polyline = p0.addPolyline(
-                    PolylineOptions()
-                        .add(
-                            LatLng(preLocation.first.toDouble(), preLocation.second.toDouble()),
-                            LatLng(location.first.toDouble(), location.second.toDouble())
+        preLocation = resultIterator.next()
+        destinationLocation = resultIterator.next()
+        val tAnimator = ValueAnimator.ofInt(0, resultLongi.size-2)
+        tAnimator.duration = 5000
+        val frameDistance = result.distance
+        tAnimator.addUpdateListener {
+            // animate here
+            tAnimator ->
+            val frame = tAnimator.animatedValue as Int
+            val polyline = p0.addPolyline(
+                PolylineOptions()
+                    .add(
+                        LatLng(resultLati[frame].toDouble(), resultLongi[frame].toDouble()),
+                        LatLng(resultLati[frame+1].toDouble(), resultLongi[frame+1].toDouble())
+                    )
+                    .addSpan(
+                        StyleSpan(
+                            StrokeStyle.gradientBuilder(
+                                Color.HSVToColor(floatArrayOf(360F*frame/(resultLati.size-1), 1F, 1F)),
+                                Color.HSVToColor(floatArrayOf(360F*(frame+1)/(resultLati.size-1), 1F, 1F))
+                            ).build()
                         )
-                )
-                polyline.width = 15.0F
-                polyline.color = Color.rgb(122, 94, 200)
-                polyline.jointType = JointType.ROUND
-                polyline.startCap = RoundCap()
-                polyline.endCap = RoundCap()
-            }
-            else{
-                val polyline = p0.addPolyline(
-                    PolylineOptions()
-                        .add(
-                            LatLng(location.first.toDouble()+0.000001, location.second.toDouble()+0.000001),
-                            LatLng(location.first.toDouble(), location.second.toDouble())
-                        )
-                )
-                polyline.width = 15.0F
-                polyline.color = Color.rgb(122, 94, 200)
-                polyline.jointType = JointType.ROUND
-                polyline.startCap = RoundCap()
-                polyline.endCap = RoundCap()
-            }
-            preLocation = location
+                    )
+            )
+            polyline.width = 15.0F
+//            polyline.color = Color.rgb(122, 94, 200)
+            polyline.jointType = JointType.ROUND
+            polyline.startCap = RoundCap()
+            polyline.endCap = RoundCap()
         }
+        tAnimator.startDelay=3000
+        tAnimator.start()
+
+//        for (location in resultLati.zip(resultLongi)) {
+//            Log.d(TAG + "위치정보", "위도: ${location.first.toDouble()} 경도: ${location.second.toDouble()}")
+//            if (preLocation != null) {
+//                Log.d(
+//                    TAG + "check",
+//                    "${location.first.toDouble()} ${location.second.toDouble()} ${preLocation.first.toDouble()} ${preLocation.second.toDouble()}"
+//                )
+//                val polyline = p0.addPolyline(
+//                    PolylineOptions()
+//                        .add(
+//                            LatLng(preLocation.first.toDouble(), preLocation.second.toDouble()),
+//                            LatLng(location.first.toDouble(), location.second.toDouble())
+//                        )
+//                )
+//                polyline.width = 15.0F
+//                polyline.color = Color.rgb(122, 94, 200)
+//                polyline.jointType = JointType.ROUND
+//                polyline.startCap = RoundCap()
+//                polyline.endCap = RoundCap()
+//            }
+//            else{
+//                val polyline = p0.addPolyline(
+//                    PolylineOptions()
+//                        .add(
+//                            LatLng(location.first.toDouble()+0.000001, location.second.toDouble()+0.000001),
+//                            LatLng(location.first.toDouble(), location.second.toDouble())
+//                        )
+//                )
+//                polyline.width = 15.0F
+//                polyline.color = Color.rgb(122, 94, 200)
+//                polyline.jointType = JointType.ROUND
+//                polyline.startCap = RoundCap()
+//                polyline.endCap = RoundCap()
+//            }
+//            preLocation = location
+//        }
     }
 }
