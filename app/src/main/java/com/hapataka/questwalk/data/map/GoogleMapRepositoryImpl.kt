19501e47871d8df221e823d8ae13f1121788a9ce
이성재 <sj5lee1@gmail.com>
2024-03-14 17:06:37 +0900

    Feat: 지도 애니메이션 구축 Fix: 스크롤뷰가 지도 사용에 방해하는 것 막음 Fix: 위치 정확도를 위한 if문 추가 Fix: 항상 위치를 한 번 받게 바꿈

--- app/src/main/java/com/hapataka/questwalk/data/map/GoogleMapRepositoryImpl.kt
+++ app/src/main/java/com/hapataka/questwalk/data/map/GoogleMapRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.hapataka.questwalk.data.map
 
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.util.Log
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,6 +12,8 @@ import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.gms.maps.model.RoundCap
+import com.google.android.gms.maps.model.StrokeStyle
+import com.google.android.gms.maps.model.StyleSpan
 import com.hapataka.questwalk.domain.entity.HistoryEntity
 import com.hapataka.questwalk.domain.repository.MapRepository
 
@@ -25,16 +28,37 @@ class GoogleMapRepositoryImpl: MapRepository, OnMapReadyCallback {
             LatLng(it.first.toDouble(), it.second.toDouble())
         }?.toMutableList() ?: mutableListOf()
 
-        googleMap?.addPolyline(
-            PolylineOptions()
-                .clickable(false)
-                .addAll(locationList)
-                .width(15.0F)
-                .color(Color.rgb(122, 94, 200))
-                .jointType(JointType.ROUND)
-                .startCap(RoundCap())
-                .endCap(RoundCap())
-        )
+        val tAnimator = ValueAnimator.ofInt(0, locationList.size-2)
+        tAnimator.duration = 5000
+        val frameDistance = result.distance
+        tAnimator.addUpdateListener {
+            // animate here
+                tAnimator ->
+            val frame = tAnimator.animatedValue as Int
+            val polyline = googleMap?.addPolyline(
+                PolylineOptions()
+                    .add(
+                        LatLng(locationList[frame].latitude.toDouble(), locationList[frame].longitude.toDouble()),
+                        LatLng(locationList[frame+1].latitude.toDouble(), locationList[frame+1].longitude.toDouble())
+                    )
+                    .addSpan(
+                        StyleSpan(
+                            StrokeStyle.gradientBuilder(
+                                Color.HSVToColor(floatArrayOf(360F*frame/(locationList.size-1), 1F, 1F)),
+                                Color.HSVToColor(floatArrayOf(360F*(frame+1)/(locationList.size-1), 1F, 1F))
+                            ).build()
+                        )
+                    )
+            )
+            polyline?.width = 15.0F
+//            polyline.color = Color.rgb(122, 94, 200)
+            polyline?.jointType = JointType.ROUND
+            polyline?.startCap = RoundCap()
+            polyline?.endCap = RoundCap()
+        }
+        tAnimator.startDelay=3000
+        tAnimator.start()
+
         addMarker(result)
         controlCamera()
     }
@@ -63,5 +87,6 @@ class GoogleMapRepositoryImpl: MapRepository, OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         googleMap = p0
+        p0.uiSettings.isZoomControlsEnabled=true
     }
 }
