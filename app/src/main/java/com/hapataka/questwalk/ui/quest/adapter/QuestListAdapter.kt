415654ca735f7e98a5397b5b1726ffbba70b782a
YS <etgt777@naver.com>
2024-03-22 16:01:25 +0900

    update ver 1.21

--- app/src/main/java/com/hapataka/questwalk/ui/quest/adapter/QuestListAdapter.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/quest/adapter/QuestListAdapter.kt
@@ -5,9 +5,7 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -15,7 +13,6 @@ import coil.load
 import com.hapataka.questwalk.R
 import com.hapataka.questwalk.databinding.ItemQuestBinding
 import com.hapataka.questwalk.ui.quest.QuestData
-import kotlin.math.round
 import kotlin.math.roundToInt
 
 class QuestListAdapter(
