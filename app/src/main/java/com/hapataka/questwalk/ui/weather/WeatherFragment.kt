f7aa490c60f0e32dc303fb217a32eb8a172df859
KJ-Hyeon <bark052526@gmail.com>
2024-06-21 02:03:21 +0900

    refactor: migrate to stateflow from livedata in weather viewmodel

--- app/src/main/java/com/hapataka/questwalk/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/weather/WeatherFragment.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.hapataka.questwalk.data.model.WeatherModel
@@ -15,6 +18,8 @@ import com.hapataka.questwalk.ui.weather.adapter.WeatherAdapterDecoration
 import com.hapataka.questwalk.ui.common.BaseFragment
 import com.hapataka.questwalk.util.LoadingDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class WeatherFragment : BaseFragment<FragmentWeatherBinding>(FragmentWeatherBinding::inflate){
@@ -32,23 +37,23 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(FragmentWeatherBind
     }
 
     private fun dataObserve() {
-        with(weatherViewModel) {
-            weatherModel.observe(viewLifecycleOwner) {weatherModel ->
-                Log.d("weatherFragment:","weatherModel: $weatherModel")
-                weatherAdapter.submitList(weatherModel.forecastList)
-                initDustViews(weatherModel)
-            }
-
-            weatherState.observe(viewLifecycleOwner) {weatherState ->
-                Log.d("weatherFragment:","weatherState: $weatherState")
-            }
-
-            error.observe(viewLifecycleOwner) {error ->
-                if (error) setErrorState()
-            }
-
-            isLoading.observe(viewLifecycleOwner) {isLoading ->
-                showLoadingDialog(isLoading)
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                weatherViewModel.weatherUiState.collectLatest { state ->
+                    when(state) {
+                        is WeatherUiState.Loading -> {
+                            showLoadingDialog(state.loading)
+                        }
+                        is WeatherUiState.Success -> {
+                            weatherAdapter.submitList(state.data.forecastList)
+                            initDustViews(state.data)
+                            Log.d("weatherFragment:","weatherState: ${state.level}")
+                        }
+                        is WeatherUiState.Error -> {
+                            setErrorState()
+                        }
+                    }
+                }
             }
         }
     }
@@ -56,20 +61,12 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(FragmentWeatherBind
     private fun initViews() {
         initRecyclerView()
         initBackButton()
-
-        weatherViewModel.getWeatherModel()
     }
 
     private fun initRecyclerView() {
         with(binding.revWeather) {
             adapter = weatherAdapter
             addItemDecoration(WeatherAdapterDecoration())
-        }
-    }
-
-    private fun initBackButton() {
-        binding.ivArrowBack.setOnClickListener {
-            navHost.popBackStack()
         }
     }
 
@@ -85,19 +82,6 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(FragmentWeatherBind
         navHost.popBackStack()
     }
 
-    private fun setWeatherPreview(weatherPreview: WeatherPreviewData) {
-        binding.tvMessage.text =
-            "현재 온도는 ${weatherPreview.currentTmp}도 이고, 하늘 상태는 ${weatherPreview.sky} ${weatherPreview.precipType} "+
-                    "미세먼지 상태는 ${weatherPreview.miseState} 초미세 먼지 상태는 ${weatherPreview.choMiseState} 오늘 여행에 참고하라구!!"
-    }
-
-    private fun setDustText(dustInfo: DustEntity) {
-        with(binding) {
-            tvMiseValue.text = if (dustInfo.pm10Value == -1) "통신 장애" else "${dustInfo.pm10Value} ㎍/㎥"
-            tvChomiseValue.text = if (dustInfo.pm25Value == -1) "통신 장애" else "${dustInfo.pm25Value} ㎍/㎥"
-        }
-    }
-
     private fun showLoadingDialog(isLoading: Boolean) {
         if (isLoading) {
             LoadingDialogFragment().show(parentFragmentManager, "loadingDialog")
@@ -107,4 +91,11 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(FragmentWeatherBind
             loadingFragment?.dismiss()
         }
     }
+
+    private fun initBackButton() {
+        binding.ivArrowBack.setOnClickListener {
+            navHost.popBackStack()
+        }
+    }
+
 }
--- app/src/main/java/com/hapataka/questwalk/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/hapataka/questwalk/ui/weather/WeatherViewModel.kt
@@ -1,14 +1,13 @@
 package com.hapataka.questwalk.ui.weather
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hapataka.questwalk.data.model.WeatherModel
 import com.hapataka.questwalk.domain.usecase.GetWeatherStateUseCase
 import com.hapataka.questwalk.domain.usecase.GetWeatherUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,44 +16,22 @@ class WeatherViewModel @Inject constructor(
     private val weatherUseCase: GetWeatherUseCase,
     private val weatherStateUseCase: GetWeatherStateUseCase
 ) : ViewModel() {
-    private val _weatherModel = MutableLiveData<WeatherModel>()
-    val weatherModel: LiveData<WeatherModel> get() = _weatherModel
+    private val _weatherUiState = MutableStateFlow<WeatherUiState>(WeatherUiState.Loading(true))
+    val weatherUiState: StateFlow<WeatherUiState> get() = _weatherUiState
 
-    private val _weatherState = MutableLiveData<Int>()
-    val weatherState: LiveData<Int> get() = _weatherState
-
-    private val _error = MutableLiveData(false)
-    val error: LiveData<Boolean> get() = _error
-
-    private val _isLoading = MutableLiveData(false)
-    val isLoading: LiveData<Boolean> get() = _isLoading
-
-    fun getWeatherModel() {
-        _isLoading.value = true
+    init {
         viewModelScope.launch {
-            val weatherModel = weatherUseCase()
-
-            weatherModel.fold(
+            _weatherUiState.value = weatherUseCase().fold(
                 onSuccess = {
-                    _weatherModel.value = it
-                    _error.value = false
-                    _isLoading.value = false
-                    getWeatherState(it)
+                    val weatherLevel = weatherStateUseCase(it)
+                    WeatherUiState.Success(it, weatherLevel)
                 },
                 onFailure = {
-                    Log.d("WeatherViewModel", "getWeatherModel: ${it.message}")
-                    _error.value = true
-                    _isLoading.value = false
+                    WeatherUiState.Error(true)
                 }
             )
+            _weatherUiState.value = WeatherUiState.Loading(false)
         }
     }
 
-        private fun getWeatherState(weatherModel: WeatherModel?) {
-            viewModelScope.launch {
-                weatherModel?.let {
-                    _weatherState.value = weatherStateUseCase(weatherModel)
-                }
-            }
-        }
-    }
+}
