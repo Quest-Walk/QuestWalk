a5b1deb56caf972a5a8e5ebefb8c3c139ccdb979
bong-pal <etgt777@naver.com>
2025-01-04 00:13:19 +0900

    Chore: cleanup build
    - version catalog에 plugin 및 공통 빌드에 필요한 library 추가
    - 프로젝트 build파일 plugin을  libs의 plugin으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.service.gms) apply false
+    alias(libs.plugins.android.application) apply  false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.hilt) apply false
 }
+
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
 }
