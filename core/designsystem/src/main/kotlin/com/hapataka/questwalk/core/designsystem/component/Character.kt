b2c90b7979535b48b24798f2f46e34bc08c36178
bong-pal <etgt777@naver.com>
2025-01-11 01:18:56 +0900

    Feat: 캐릭터 컴포넌트 추가
    - Boolean값에 따라 애니메이션 여부 결정
    - InfiniteTransition을 사용해 직접 애니메이션 구현

---
+++ core/designsystem/src/main/kotlin/com/hapataka/questwalk/core/designsystem/component/Character.kt
@@ -0,0 +1,71 @@
+package com.hapataka.questwalk.core.designsystem.component
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.res.painterResource
+import com.hapataka.questwalk.core.designsystem.R
+
+enum class Character(val images: List<Int>) {
+    BEAR(
+        listOf(
+            R.drawable.img_bear_s,
+            R.drawable.img_bear_l1,
+            R.drawable.img_bear_l2,
+            R.drawable.img_bear_l1,
+            R.drawable.img_bear_s,
+            R.drawable.img_bear_r1,
+            R.drawable.img_bear_r2,
+            R.drawable.img_bear_r1,
+        )
+    ),
+}
+
+@Composable
+fun Character(
+    character: Character = Character.BEAR,
+    isAnimate: Boolean = false,
+    modifier: Modifier = Modifier,
+) {
+    val frame = character.images.map {
+        painterResource(it)
+    }
+
+    val currentFrame by rememberInfiniteTransition(character.name).animateValue(
+        initialValue = 0,
+        targetValue = frame.size,
+        typeConverter = Int.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = character.name
+    )
+
+    if (isAnimate) {
+        Box(
+            modifier = modifier
+                .drawBehind {
+                    with(frame[currentFrame]) {
+                        draw(size = size)
+                    }
+                },
+        )
+    } else {
+        Image(
+            painter = frame[0],
+            contentDescription = null,
+            modifier = modifier
+        )
+    }
+}
