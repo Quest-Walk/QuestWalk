49f00d8ba92f43cd202a7d5678b3984500c16b15
bong-pal <etgt777@naver.com>
2025-02-19 02:18:08 +0900

    Chore: room library 종속성 추가

--- core/local/build.gradle.kts
+++ core/local/build.gradle.kts
@@ -3,10 +3,15 @@ import com.hapataka.questwalk.setNamespace
 plugins {
     id("questwalk.android.library")
     id("questwalk.android.hilt")
+    alias(libs.plugins.ksp)
 }
 
 setNamespace("core.local")
 
 dependencies {
     implementation(projects.core.domain)
+
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
