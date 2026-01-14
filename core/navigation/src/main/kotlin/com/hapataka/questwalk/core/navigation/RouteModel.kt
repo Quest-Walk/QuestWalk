4a383111606ff93b9f03e8fd781a0a68f2258a96
bong-pal <etgt777@naver.com>
2026-01-14 23:08:42 +0900

    feat: MainRoute, HomeRoute Navigation Route 추가
    - MainRoute: 메인 앱 화면들 (Home, Quest, Record, Weather, MyInfo, Camera)
    - HomeRoute: Home에서 파생되는 상세 화면 (QuestDetail, Result)
    - Type-safe Navigation 준비

--- core/navigation/src/main/kotlin/com/hapataka/questwalk/core/navigation/RouteModel.kt
+++ core/navigation/src/main/kotlin/com/hapataka/questwalk/core/navigation/RouteModel.kt
@@ -21,3 +21,39 @@ sealed interface OnboardingStep {
     @Serializable
     data object Setup : OnboardingStep
 }
+
+/**
+ * 메인 앱 화면 Route
+ */
+@Serializable
+sealed interface MainRoute {
+    @Serializable
+    data object Home : MainRoute
+
+    @Serializable
+    data object Quest : MainRoute
+
+    @Serializable
+    data object Record : MainRoute
+
+    @Serializable
+    data object Weather : MainRoute
+
+    @Serializable
+    data object MyInfo : MainRoute
+
+    @Serializable
+    data object Camera : MainRoute
+}
+
+/**
+ * Home 화면에서 파생되는 상세 Route
+ */
+@Serializable
+sealed interface HomeRoute {
+    @Serializable
+    data class QuestDetail(val keyword: String) : HomeRoute
+
+    @Serializable
+    data class Result(val resultId: String) : HomeRoute
+}
